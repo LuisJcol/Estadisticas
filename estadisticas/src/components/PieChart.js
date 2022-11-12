26c482b1b09c9c76af59177e03412dd382dcb4b3
crigores <christopher.rigores@gmail.com>
2022-11-12 04:16:13 -0400

    aiuda se me crasheo el endpoint

--- estadisticas/src/components/PieChart.js
+++ estadisticas/src/components/PieChart.js
@@ -1,51 +1,69 @@
-import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend} from "chart.js";
+import { useEffect, useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
-
-
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
+export default function PieChart(props) {
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    showData();
+  }, [])
+
+  const showData = () => {
+    let facultadArray = [];
+    let inscripcionesArray = [];
+    props.facultades.map(facultad => {
+      facultadArray.push(facultad.facultad);
+      return facultad;
+    })
+    console.log("inscripciones", props.inscripciones);
+    props.inscripciones.map(inscripcion => {
+      inscripcionesArray.push(inscripcion.estudiantes);
+      return inscripcion;
+    });
+
+    let data = {
+      labels: facultadArray,
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
       ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+    };
 
-function PieChart  () {
-    return(
-        <div>
-            <h1>Estudiantes inscritos por </h1>
-            <div style={{width: "350px", height: "350px" }}>
-                <Pie data={data} />
-            </div>
-        </div>
+    setData(data);
+    setShow(true);
 
-    );
-    
-        
+  }
 
+
+  return <>
+    {show && <div>
+      <h1>Estudiantes inscritos por {props.message}</h1>
+      <div style={{ width: "350px", height: "350px" }}>
+        <Pie data={data} />
+      </div>
+    </div>}
+  </>;
 }
-
-export default PieChart;
--- estadisticas/src/view/Reports/Report.js
+++ estadisticas/src/view/Reports/Report.js
@@ -10,92 +10,132 @@ import PieChart from "../../components/PieChart";
 
 const Report = () => {
 
-    const baseUrl = "http://localhost/Backend2/index.php?c=escuelas&a=verFacultades";
-    // const baseUrl2 = "http://localhost/Backend2/index.php?c=escuelas&a=verEscuela";
-    const baseUrl3 = "http://localhost/Backend2/index.php?c=periodos&a=verPeriodo";
-    // const [data, setData]=useState([]);
-    const [datafacultades, setDataFacultades]=useState([]);
-    const [dataescuela, setDataEscuela]=useState([]);
-    const [dataperiodos, setDataPeriodo]=useState([]);
+	// Consulta para obtener el listado de facultades
+	const baseUrl = "http://localhost/Backend2/index.php?c=escuelas&a=verFacultades";
+	// Consulta para obtener el listado de Inscripciones por escuela
+	const baseUrl2 = "http://localhost/Backend2/index.php?c=inscripciones&a=verInscripcionesEscuela";
+	// Consulta para obtener el listado de periodos
+	const baseUrl3 = "http://localhost/Backend2/index.php?c=periodos&a=verPeriodo";
+	// const [data, setData]=useState([]);
 
-    const getFacultad = async()=>{
-        await axios.get(baseUrl)
-        .then(response=>{
-            setDataFacultades(response.data)
-        })
-    }
+	// const dataEscuelas = [
+	// 	{ escuela: "Informatica" },
+	// 	{ escuela: "Industrial" },
+	// 	{ escuela: "Telecomunicaciones" },
+	// 	{ escuela: "Automatizacion" },
+	// 	{ escuela: "Audiovisual" },
+	// 	{ escuela: "Periodismo" },
+	// 	{ escuela: "RelacionesPublicas" },
+	// 	{ escuela: "Contaduria" },
+	// 	{ escuela: "AdmEmpresas" },
+	// 	{ escuela: "Mercadeo" },
+	// 	{ escuela: "BancaSeguros" },
+	// 	{ escuela: "RelacionesIndustriales" },
+	// 	{ escuela: "Derecho" },
+	// 	{ escuela: "Diseno" }
+	// ];
 
-    const getEscuela = async()=>{
-        await axios.get(baseUrl)
-        .then(response=>{
-            setDataEscuela(response.data)
-        })
-    }
+	const [datafacultades, setDataFacultades] = useState([]);
+	const [dataperiodos, setDataPeriodo] = useState([]);
+	const [inscripcionEscuela, setInscripcionEscuela] = useState([]);
 
-    const getPeriodo = async()=>{
-        await axios.get(baseUrl3)
-        .then(response=>{
-            setDataPeriodo(response.data)
-        })
-    }
+	const [modo, setModo] = useState("")
+	const [facultadSelected, setFacultadSelected] = useState("");
+	const [periodoSelected, setPeriodoSelected] = useState(0);
 
-    useEffect(()=>{
-        getFacultad();
-        getEscuela();
-        getPeriodo();
-    },[])
+	const getFacultad = async () => {
+		await axios.get(baseUrl)
+			.then(response => {
+				setDataFacultades(response.data);
+			})
+	}
 
-    return(
-        <>
-            <Header/>
-                <Container>
-                    <Row className="mt-5">
-                        
-                        <Col xl="12" md="10" lg="10" sm="8">
-                            <Row>
-                                <form>
-                                    <select className="form-control" name="facultad" id="facultad">
-                                        <option value={0}>Seleccione una opcion</option>
-                                        {datafacultades.map(facultad=>(
-                                            <option key={facultad.id_facultad} value={facultad.id_facultad}>{facultad.facultad}</option>
-                                        ))}
-                                    </select> <br/>
-                                    <select className="form-control" name="escuela" id="escuela">
-                                        <option value={0}>Seleccione una opcion</option>
-                                        {dataescuela.map(escuela=>(
-                                            <option key={escuela.id_escuela} value={escuela.id_escuela}>{escuela.escuela}</option>
-                                        ))}
-                                    </select><br/>
-                                    <select className="form-control" name="periodo" id="periodo">
-                                        <option value={0}>Seleccione una opcion</option>
-                                        {dataperiodos.map(periodo=>(
-                                            <option key={periodo.id_periodo} value={periodo.id_periodo}>{periodo.abreviatura_periodo}</option>
-                                        ))}
-                                    </select>
+	const getInscripcionEscuela = async () => {
+		await axios.get(baseUrl2)
+			.then(response => {
+				console.log(response.data)
+				setInscripcionEscuela(response.data)
+			})
 
-                                </form>
-                            </Row>
-                            
-                            
-                            <Row className="py-5">
-                                
-                                <Col xl="6" md="6">
-                                    <DoughnutChart/>
-                                </Col>
-                                <Col xl="6" md="6">
-                                
-                                    <PieChart/>
-                            
-                                </Col>
+	}
 
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container> 
-            
-            <Footer/>
-        </>
-    );
+	const getPeriodo = async () => {
+		await axios.get(baseUrl3)
+			.then(response => {
+				setDataPeriodo(response.data);
+			})
+	}
+
+	const Modalidad = event => setModo(event.target.value);
+	const SelectFacultad = event => setFacultadSelected(event.target.value);
+	const SelectPeriodo = event => setPeriodoSelected(event.target.value);
+
+	useEffect(() => {
+		getFacultad();
+		getInscripcionEscuela();
+		getPeriodo();
+	}, [])
+
+	return (
+		<>
+			<Header />
+			<Container>
+				<Row className="mt-5">
+					<Col xl="12" md="10" lg="10" sm="8">
+						<Row>
+							<form>
+
+								<select className="form-control" name="opcion" id="opcion" onChange={Modalidad}>
+									<option value="">Seleccione una Opcion</option>
+									<option value="facultad">Por Facultad</option>
+									<option value="escuela">Por Escuela</option>
+								</select> <br />
+
+
+								{modo === "facultad" && <>
+									<select className="form-control" name="facultad" id="facultad" onChange={SelectFacultad}>
+										<option value={0}>Seleccione una Facultad</option>
+										{datafacultades.map(facultad => (
+											<option key={facultad.id_facultad} value={facultad.facultad}>{facultad.facultad}</option>
+										))}
+									</select>
+									<br />
+								</>}
+
+								{/* {modo === "escuela" && <>
+									<select className="form-control" name="facultad" id="facultad" onChange={SelectFacultad}>
+										<option value={0}>Seleccione una Escuela</option>
+										{dataEscuelas.map(escuela => (
+											<option key={escuela.escuela} value={escuela.escuela}>{escuela.escuela}</option>
+										))}
+									</select><br />
+								</>} */}
+
+
+								{facultadSelected.length > 0 ? <select className="form-control" name="periodo" id="periodo" onChange={SelectPeriodo}>
+									<option value={0}>Seleccione una un Periodo</option>
+									{dataperiodos.map(periodo => (
+										<option key={periodo.id_periodo} value={periodo.id_periodo}>{periodo.abreviatura_periodo}</option>
+									))}
+								</select> : null}
+							</form>
+						</Row>
+
+						{periodoSelected > 0 ? <Row className="py-5">
+							<Col xl="6" md="6">
+								<DoughnutChart facultades={datafacultades} inscripciones={inscripcionEscuela} message={modo} />
+							</Col>
+							<Col xl="6" md="6">
+								<PieChart facultades={datafacultades} inscripciones={inscripcionEscuela} message={modo} />
+							</Col>
+						</Row> : null}
+					</Col>
+				</Row>
+			</Container>
+
+			<Footer />
+		</>
+	);
 }
 
 
